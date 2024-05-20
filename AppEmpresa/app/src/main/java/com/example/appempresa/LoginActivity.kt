25a2be940b917011f756e861076466b59ce9f5f9
Rubenmixeep <Ruben-Maurin1@eep-igroup.com>
2024-05-20 12:14:23 +0200

    proyecto

--- AppEmpresa/app/src/main/java/com/example/appempresa/LoginActivity.kt
+++ AppEmpresa/app/src/main/java/com/example/appempresa/LoginActivity.kt
@@ -3,31 +3,53 @@
 package com.example.appempresa
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.appempresa.ui.theme.AppEmpresaTheme
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
-class MainActivity : ComponentActivity() {
+class LoginActivity : ComponentActivity() {
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
-        val buttonRegister = findViewById<Button>(R.id.button3)
-        buttonRegister.setOnClickListener {
-        val intent = Intent(this@MainActivity, RegisterActivity::class.java)
-            startActivities(arrayOf(intent))
 
-        }
     }
+
 }
+    fun CheckUser (context: Context, email:String, password:String){
+        Text(text = )
+        val url="http://127.0.0.1:9124/main/userlogin"
+        val rqueue = Volley.newRequestQueue(context)
+        val json = JSONObject()
+        json.put("email", email)
+        json.put("password", password)
+        val request = JsonObjectRequest(
+            Request.Method.POST, url, json,
+            { response ->
+                if (response.has("id") && response.has("name")) {
+                    val id = response.getString("id")
+                    val nombre = response.getString("name")
+                    val intent = Intent(context, TareaActivity::class.java)
+                    // Puedes pasar datos adicionales al intent si es necesario
+                    intent.putExtra("id", id)
+                    intent.putExtra("nombre", nombre)
+                    context.startActivity(intent)
+
+                } else {
+                    Toast.makeText(context, "No 'id' field found in JSON response", Toast.LENGTH_SHORT).show()
+                }
+            },
+            { error ->
+                Toast.makeText(context, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+        )
+
+        rqueue.add(request)
+    }
--- AppEmpresa/app/src/main/java/com/example/appempresa/RegisterActivity.kt
+++ AppEmpresa/app/src/main/java/com/example/appempresa/RegisterActivity.kt
@@ -2,19 +2,10 @@ package com.example.appempresa
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.appempresa.ui.theme.AppEmpresaTheme
 
-class MainActivity2 : ComponentActivity() {
+class RegisterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.destino)
+        setContentView(R.layout.register)
     }
 }
